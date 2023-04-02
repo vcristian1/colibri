02136f8e8c56a8d6e69d52439ef88ac40178ff1d
Cristian Vargas <cristian.v0223@gmail.com>
2023-04-01 21:01:31 -0500

    switched images in arrray

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -26,13 +26,13 @@ const classes: Array<ClassType> = [
     name: "Painting",
     description:
       "A living room in the Chicagoland area plastered and painted by Colibri in 2023.",
-    image: image3,
+    image: image4,
   },
   {
     name: "Kitchen Renovation",
     description:
       "A gut kitchen renovation in the Chicagoland area completed by Colibri in 2021.",
-    image: image4,
+    image: image3,
   },
   {
     name: "Flooring",
